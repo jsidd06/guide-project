ae4b70480a47ee5fb8e1f36db3f21db23ec0825c
siddharth <jsidd06@gmail.com>
2022-02-28 10:38:58 +0530

    add date the sperate components add css also

--- src/Components/ExpenseItem/ExpenseItem.js
+++ src/Components/ExpenseItem/ExpenseItem.js
@@ -1,16 +1,11 @@
 import React from 'react'
+import ExpenseDate from '../ExpenseDate/ExpenseDate';
 import "../ExpenseItem/ExpenseItem.css"
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString("en-IN", { month: "long" });
-  const day = props.date.toLocaleString("en-IN", { day: "2-digit" });
-  const year = props.date.getFullYear();
+ 
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
       </div>

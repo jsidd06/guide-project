14e83fa34993b8543943937dcbc0d28489fdc84d
siddharth <jsidd06@gmail.com>
2022-02-28 10:11:04 +0530

    adding a logic to date month or year dynmic

--- src/App.js
+++ src/App.js
@@ -10,18 +10,18 @@ function App() {
     id: 2,
     title: "Rent",
     amount: 1095.00,
-    date: new Date(2021, 2, 28)
+    date: new Date(2021, 3, 21)
   },{
     id: 3,
     title: "Credit Card",
     amount: 60.00,
-    date: new Date(2021, 2, 28)
+    date: new Date(2021, 4, 27)
     
   },{
     id: 4,
     title: "Grocery",
     amount: 99.99,
-    date: new Date(2021, 2, 28)
+    date: new Date(2021, 5, 22)
   }]
   return (
     <div>

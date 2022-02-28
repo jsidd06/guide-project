adb8bd8adf1151f08ff804c907c972a2bcbb563f
siddharth <jsidd06@gmail.com>
2022-02-28 11:50:40 +0530

    add expenses file to sperate the file and add with props components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import ExpenseItem from "./Components/ExpenseItem/ExpenseItem";
-
+import Expense from "./Components/Expense/Expense";
 function App() {
   const expense = [{
     id: 1,
@@ -26,26 +25,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expense[0].title}
-        amount={expense[0].amount}
-        date={expense[0].date}
-      />
-      <ExpenseItem
-        title={expense[1].title}
-        amount={expense[1].amount}
-        date={expense[1].date}
-      />
-      <ExpenseItem
-        title={expense[2].title}
-        amount={expense[2].amount}
-        date={expense[2].date}
-      />
-      <ExpenseItem
-        title={expense[3].title}
-        amount={expense[3].amount}
-        date={expense[3].date}
-      />
+      <Expense items={expense} />
     </div>
   );
 }

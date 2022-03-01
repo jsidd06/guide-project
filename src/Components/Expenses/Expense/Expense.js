b5054d00dc771c471505c58e84e848665d8ca225
siddharth <jsidd06@gmail.com>
2022-03-01 15:17:32 +0530

    add the data in list file and set condtion and add css

--- src/Components/Expenses/Expense/Expense.js
+++ src/Components/Expenses/Expense/Expense.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Card from "../../UI/Card/Card";
 import ExpenseFilter from "../ExpenseFilter/ExpenseFilter";
-import ExpenseItem from "../ExpenseItem/ExpenseItem";
+import ExpenseList from "../ExpenseList/ExpenseList";
 import "./Expense.css";
 const Expense = (props) => {
   const [selectedFilterYear, setSelectedFilterYear] = useState("2022");
@@ -13,17 +13,7 @@ const Expense = (props) => {
     return expense.date.getFullYear().toString() === selectedFilterYear;
   });
 
-  let expenseList = <p>No Expense found.</p>;
-  if (filterExpense.length > 0) {
-   expenseList = filterExpense.map((expense) => (
-            <ExpenseItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          ))
-  }
+  
   return (
     <div>
       <Card className="expenses">
@@ -31,7 +21,7 @@ const Expense = (props) => {
           selected={selectedFilterYear}
           onChangeFilter={filterChangeHandler}
         />
-        {expenseList}
+        <ExpenseList items={filterExpense} />
       </Card>
     </div>
   );
--- src/Components/Expenses/ExpenseItem/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem/ExpenseItem.js
@@ -9,14 +9,16 @@ const  ExpenseItem = (props) => {
     setTitle("updated");
   }
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-      </div>
-      <div className="expense-item__price">${props.amount}</div>
-      <button onClick={clickHandler}>Change title</button>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{title}</h2>
+        </div>
+        <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler}>Change title</button>
+      </Card>
+    </li>
   );
 }
 

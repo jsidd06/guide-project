0573f0743e2f928aecffb6348e615fee902d790e
siddharth <jsidd06@gmail.com>
2022-03-01 12:27:23 +0530

    add a map function to pass the all item

--- src/Components/Expenses/Expense/Expense.js
+++ src/Components/Expenses/Expense/Expense.js
@@ -1,40 +1,30 @@
-import React, { useState } from 'react'
-import Card from '../../UI/Card/Card';
-import ExpenseFilter from '../ExpenseFilter/ExpenseFilter';
-import ExpenseItem from '../ExpenseItem/ExpenseItem'
-import "./Expense.css"
+import React, { useState } from "react";
+import Card from "../../UI/Card/Card";
+import ExpenseFilter from "../ExpenseFilter/ExpenseFilter";
+import ExpenseItem from "../ExpenseItem/ExpenseItem";
+import "./Expense.css";
 const Expense = (props) => {
-  const [selectedFilterYear,setSelectedFilterYear] = useState("2022");
-  const filterChangeHandler = selectedFilter => {
-   setSelectedFilterYear(selectedFilter);
-  }
+  const [selectedFilterYear, setSelectedFilterYear] = useState("2022");
+  const filterChangeHandler = (selectedFilter) => {
+    setSelectedFilterYear(selectedFilter);
+  };
   return (
     <div>
       <Card className="expenses">
-      <ExpenseFilter selected={selectedFilterYear} onChangeFilter={filterChangeHandler} />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
+        <ExpenseFilter
+          selected={selectedFilterYear}
+          onChangeFilter={filterChangeHandler}
         />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
-}
+};
 
-export default Expense
+export default Expense;

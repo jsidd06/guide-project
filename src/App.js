792ad4d2e0f0aa9c469093a44418dde737e23bcb
siddharth <jsidd06@gmail.com>
2022-03-01 13:23:14 +0530

    add the in state and set new items dyanamic

--- src/App.js
+++ src/App.js
@@ -1,39 +1,48 @@
+import { useState } from "react";
 import Expense from "./Components/Expenses/Expense/Expense";
 import NewExpense from "./Components/Expenses/NewExpense/NewExpense";
 
-const App = () => {
-  const expense = [{
+
+const DUMMY_EXPENSE = [
+  {
     id: 1,
     title: "Car Insurance",
     amount: 294.67,
-    date: new Date(2021, 2, 28)
-  },{
+    date: new Date(2021, 2, 28),
+  },
+  {
     id: 2,
     title: "Rent",
-    amount: 1095.00,
-    date: new Date(2021, 3, 21)
-  },{
+    amount: 1095.0,
+    date: new Date(2021, 3, 21),
+  },
+  {
     id: 3,
     title: "Credit Card",
-    amount: 60.00,
-    date: new Date(2021, 4, 27)
-    
-  },{
+    amount: 60.0,
+    date: new Date(2021, 4, 27),
+  },
+  {
     id: 4,
     title: "Grocery",
     amount: 99.99,
-    date: new Date(2021, 5, 22)
-  }]
+    date: new Date(2021, 5, 22),
+  },
+];
 
- const addExpenseHandler = expense => {
-   console.log(expense);
-   console.log("In App.js");
+
+const App = () => {
+  const [expenses,setExpenses] = useState(DUMMY_EXPENSE);
+ const addExpenseHandler = (expense) => {
+   setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+   });
  }
 
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expense items={expense} />
+      <Expense items={expenses} />
     </div>
   );
 }

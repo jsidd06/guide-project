a56c14d08be81a8d3d4a7d8aca0dfb0dcace4876
siddharth <jsidd06@gmail.com>
2022-03-01 15:34:14 +0530

    add button to cancel and set the state and pass the on click function to use props method

--- src/Components/Expenses/NewExpense/NewExpense.js
+++ src/Components/Expenses/NewExpense/NewExpense.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ExpenseForm from '../ExpenseForm/ExpenseForm'
 import "./NewExpense.css"
 const NewExpense = (props) => {
+  const [isEdit, setIsEdit] = useState(false);
   const saveExpenseData = (enterExpenseData) => {
     const expenseData = {
       ...enterExpenseData,
       id : Math.random().toString()
     }
     props.onAddExpense(expenseData);
+    setIsEdit(false);
+  }
+  const startEditHandler = (event) => {
+    event.preventDefault();
+    setIsEdit(true);
+  }
+  const stopEditHandler = () => {
+    setIsEdit(false);
   }
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseData} />
+      {!isEdit && <button onClick={startEditHandler}>Add New Expense</button>}
+      {isEdit && <ExpenseForm onSaveExpenseData={saveExpenseData} onCancel={stopEditHandler} />}
     </div>
   );
 }
